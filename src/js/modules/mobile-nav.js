18b189c559d3031609f73145237418f29879e1b6
S.Zelensky <zelenstein@gmail.com>
2025-07-18 16:28:39 +0300

    nav done

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,27 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtn = document.querySelector('.mobile-nav-btn');
+  const nav = document.querySelector('.mobile-nav');
+  const menuIcon = document.querySelector('.nav-icon');
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  const closeMenu = () => {
+    nav.classList.remove('mobile-nav--open');
+    menuIcon.classList.remove('nav-icon--active');
+    document.body.classList.remove('no-scroll');
+  };
+
+  navBtn.onclick = function() {
+    nav.classList.toggle('mobile-nav--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+  };
+
+  // Автоматически закрывать меню при ширине >768
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      closeMenu();
+    }
+  });
 }
 
 export default mobileNav;
